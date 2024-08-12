c6c01f4d5cb23eeeae603626a82b699599d6c0fd
Bernal <bernaljosecsfn3@gmail.com>
2024-08-11 23:42:04 -0500

    PrimerComit

---
+++ Semana2/src/main/java/com/mycompany/semana2/Semana2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana2;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author BernalXFK
+ */
+public class Semana2 {
+
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        Interfaz_grafica_factura prueba = new Interfaz_grafica_factura();
+        prueba.setVisible(true);
+    }
+}
